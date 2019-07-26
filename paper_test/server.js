4e6e62a199bf13077a9972e3b7b9fe60c44461eb
ogallagher <owengall@icloud.com>
2019-07-25 21:53:48 -0400

    Restructured project

--- paper_test/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-const port = 8080;
-const ip = "127.0.0.1";
-app.use(express.static("public"));
-
-app.listen(port,ip);
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static('public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
