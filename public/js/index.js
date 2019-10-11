9c5dfd88d80839b01353eab7ab297baf716ffbfb
ogallagher <owengall@icloud.com>
2019-10-10 23:42:05 -0400

    Improved rating widget interaction

--- public/js/index.js
+++ public/js/index.js
@@ -21,46 +21,38 @@ window.onresize = function() {
 //handle interaction with featured star buttons
 function index_featuredStars() {
 	//list of star buttons
-	var stars = $('#featured_rating').children();
+	var rating = $('#featured_rating');
+	var stars = rating.children();
 	var one = $('#featured_rating_1');
 	var two = $('#featured_rating_2');
 	var three = $('#featured_rating_3');
 	var four = $('#featured_rating_4');
 	var five = $('#featured_rating_5');
 	
-	one.mouseenter(function() {
-		one.removeClass('text-gray').addClass('text-warning');
-	}).mouseleave(function() {
+	rating.mousemove(function(event) {
+		var offset = one.offset();
+		var x = event.pageX - offset.left;
+		
+		var r = x / $(this).width();
+		
 		stars.removeClass('text-warning').addClass('text-gray');
+		
+		one.removeClass('text-gray').addClass('text-warning');
+		if (r > 1/5) {
+			two.removeClass('text-gray').addClass('text-warning');
+		}
+		if (r > 2/5) {
+			three.removeClass('text-gray').addClass('text-warning');
+		}
+		if (r > 3/5) {
+			four.removeClass('text-gray').addClass('text-warning');
+		}
+		if (r > 4/5) {
+			five.removeClass('text-gray').addClass('text-warning');
+		}
 	});
 	
-	two.mouseenter(function() {
-		one.removeClass('text-gray').addClass('text-warning');
-		two.removeClass('text-gray').addClass('text-warning');
-	}).mouseleave(function() {
-		stars.removeClass('text-warning').addClass('text-gray');
-	});
-	
-	three.mouseenter(function() {
-		one.removeClass('text-gray').addClass('text-warning');
-		two.removeClass('text-gray').addClass('text-warning');
-		three.removeClass('text-gray').addClass('text-warning');
-	}).mouseleave(function() {
-		stars.removeClass('text-warning').addClass('text-gray');
-	});
-	
-	four.mouseenter(function() {
-		one.removeClass('text-gray').addClass('text-warning');
-		two.removeClass('text-gray').addClass('text-warning');
-		three.removeClass('text-gray').addClass('text-warning');
-		four.removeClass('text-gray').addClass('text-warning');
-	}).mouseleave(function() {
-		stars.removeClass('text-warning').addClass('text-gray');
-	});
-	
-	five.mouseenter(function() {
-		stars.removeClass('text-gray').addClass('text-warning');
-	}).mouseleave(function() {
+	rating.mouseleave(function() {
 		stars.removeClass('text-warning').addClass('text-gray');
 	});
 }
