6e1fe55d95d8624c68121fcda8934e04522be69c
Owen Gallagher <gallaoj16@libmini-13.local>
2019-10-16 12:11:39 -0400

    Added some placeholder js methods to handle featured buttons interaction

--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,9 @@ window.onload = function() {
 		dbclient_fetchPuzzles(puzzle_onload);
 	});
 	
-	index_featuredStars();
+	index_featured_authors();
+	index_featured_date();
+	index_featured_stars();
 	
 	html_imports_onload();
 }
@@ -19,7 +21,7 @@ window.onresize = function() {
 }
 
 //handle interaction with featured star buttons
-function index_featuredStars() {
+function index_featured_stars() {
 	//list of star buttons
 	var rating = $('#featured_rating');
 	var stars = rating.children();
@@ -28,31 +30,58 @@ function index_featuredStars() {
 	var three = $('#featured_rating_3');
 	var four = $('#featured_rating_4');
 	var five = $('#featured_rating_5');
+	var r = 0;
 	
 	rating.mousemove(function(event) {
 		var offset = one.offset();
 		var x = event.pageX - offset.left;
 		
-		var r = x / $(this).width();
+		r = (x / $(this).width()) * 5;
 		
 		stars.removeClass('text-warning').addClass('text-gray');
 		
 		one.removeClass('text-gray').addClass('text-warning');
-		if (r > 1/5) {
+		if (r > 1) {
 			two.removeClass('text-gray').addClass('text-warning');
 		}
-		if (r > 2/5) {
+		if (r > 2) {
 			three.removeClass('text-gray').addClass('text-warning');
 		}
-		if (r > 3/5) {
+		if (r > 3) {
 			four.removeClass('text-gray').addClass('text-warning');
 		}
-		if (r > 4/5) {
+		if (r > 4) {
 			five.removeClass('text-gray').addClass('text-warning');
 		}
 	});
 	
 	rating.mouseleave(function() {
+		r = 0;
 		stars.removeClass('text-warning').addClass('text-gray');
+	});
+	
+	rating.click(function(event) {
+		alert('rating = ' + Math.floor(r+1));
+	})
+}
+
+//handle interaction with featured author buttons
+function index_featured_authors() {
+	var authors = $('#featured_authors').children();
+	
+	authors.click(function(event) {
+		var author = $(this).html().trim();
+		
+		$(this).html(author + ' *');
+	});
+}
+
+function index_featured_date() {
+	var date_button = $('#featured_date');
+	
+	date_button.click(function(event) {
+		var date = date_button.html();
+		
+		date_button.html(date + '-*');
 	});
 }
